8d86364c022e4b1ce557dd90d8bda607432eee88
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-04-02 21:06:12 +0200

    Implement login validate

---
+++ src/components/LogValidation.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class LogValidation extends Component {
+
+    render() {
+       return (
+            <form>
+                <label>Login:</label>
+                <input></input>
+                <label>Password:</label>
+                <input></input>
+                <a className="waves-effect waves-light btn">button</a> 
+            </form>  
+                     
+        );
+    }
+} 
+export default LogValidation;
