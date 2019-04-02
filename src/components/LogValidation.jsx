f04cf4232c58790c70f2e67a02065b6fc3fa9802
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-04-02 21:43:17 +0200

    Add css.

--- src/components/LogValidation.jsx
+++ src/components/LogValidation.jsx
@@ -2,14 +2,30 @@ import React, { Component } from "react";
 
 class LogValidation extends Component {
 
+    stylesLabel = {
+        color:"green",
+        fontSize:20
+    };
+
+    stylesForm = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        width: '20%',
+        marginLeft: '35%'
+    };
+
     render() {
        return (
-            <form>
-                <label>Login:</label>
+            <form style={this.stylesForm}>
+                <label style={this.stylesLabel}>Login:</label>
                 <input></input>
-                <label>Password:</label>
+                <br/>
+                <label style={this.stylesLabel}>Password:</label>
                 <input></input>
-                <a className="waves-effect waves-light btn">button</a> 
+                <br/>
+                <a className="waves-effect waves-light btn">Log in</a> 
             </form>  
                      
         );
