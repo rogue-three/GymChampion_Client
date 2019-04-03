bc9805b670ce083e6ca956fa2faec1b6d3002b3e
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-04-03 11:04:10 +0200

    Implement change login and password in state by inputs in LogValidation.

--- src/components/login/LogValidation.jsx
+++ src/components/login/LogValidation.jsx
@@ -15,22 +15,45 @@ class LogValidation extends Component {
         width: '20%',
     };
 
+    state = {
+        login:"",
+        password:""
+    }
+
+    getLoginAndPassword = () => {
+        console.log("login:" + this.state.login);
+        console.log("password:" + this.state.password);
+    }
+
+    loginChange = (event) => {
+        this.setState({login: event.target.value})
+        
+      }
+
+    passwordChange = (event) => {
+        this.setState({password: event.target.value})
+        
+      }
+
+    
+
     render() {
        return (
             <form style={this.stylesForm}>
                 <label style={this.stylesLabel}>Login:</label>
-                <input></input>
+                <input type="text" name="login" placeholder="Enter Your Login..." 
+                value={this.state.login} onChange={this.loginChange}></input>
                 <br/>
                 <label style={this.stylesLabel}>Password:</label>
-                <input></input>
+                <input type="text" name="password" placeholder="Enter Your Password..." 
+                value={this.state.password} onChange={this.passwordChange}></input>
                 <br/>
-                <a className="waves-effect waves-light btn">Log in</a> 
+                <a className="waves-effect waves-light btn"
+                 onClick={this.getLoginAndPassword}>Log in</a> 
                 <br/>
-                <a href="/sign_in">If You haven't got account sign in !</a>
+                <a>If You haven't got account sign in !</a>
                 <br/>
-            </form>  
-
-                     
+            </form>            
         );
     }
 } 
