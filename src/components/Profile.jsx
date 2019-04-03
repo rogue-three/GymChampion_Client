a6944928662e8ddffca4723e517afa9e2a519201
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-03 08:40:32 +0200

    Add Materialize dependency Add Materialize imports into index.html Color body background Implement basic Profile form

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,1 +1,48 @@
 import React, { Component } from "react";
+import avatar from "./../img/avatar.svg";
+
+class Profile extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <img src={avatar} width="10%" color="white" alt="avatar" />
+        <form>
+          <label>
+            <span className="white-text">Body weight:</span>
+            <input className="input-field white" type="text" name="name" />
+          </label>
+          <label>
+            <span className="white-text">Age:</span>
+            <input className="input-field white" type="text" name="name" />
+          </label>
+          <label>
+            <span className="white-text">Gender:</span>
+            <br />
+            <br />
+            <label>
+              <input name="gender" type="radio" checked />
+              <span className="white-text">female</span>
+            </label>
+            <br />
+            <label>
+              <input name="gender" type="radio" />
+              <span className="white-text">male</span>
+            </label>
+            <br />
+            <br />
+          </label>
+          <button
+            className="btn waves-effect waves-light"
+            type="submit"
+            name="action"
+          >
+            SAVE
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Profile;
