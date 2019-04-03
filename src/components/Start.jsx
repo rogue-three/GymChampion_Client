2a724a9d9c0b969b05165e69e0ffbb0a701afe15
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-04-03 18:36:15 +0200

    Add truck workout label with edit button to Start component.

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -4,13 +4,29 @@ class Start extends Component {
 
 styleButtonTrain = {
     width:"100%",
- 
 }
+
+styleTrackWorkout = {
+    width:"100%",
+    display:"flex",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor:"DarkTurquoise",
+    color:"white",
+    fontStyle:"italic"
+}
+
 
     render() {
         return (
         <div>
-            <h1>Example start of App</h1>
+            <div style={this.styleTrackWorkout} >
+                <h1>TRACK WORKOUT  </h1>
+                <a class="waves-effect waves-light btn-large">Edit</a>
+
+            </div>
+            
             <a style={this.styleButtonTrain} className="btn-floating btn-large pulse">
             Just start training now!</a>
         </div>
