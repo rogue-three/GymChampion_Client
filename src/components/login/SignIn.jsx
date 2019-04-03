de096815d152dd955a165fefb0fe88187afde2c5
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-04-03 14:10:12 +0200

    Implement get data from user in signIn.

--- src/components/login/SignIn.jsx
+++ src/components/login/SignIn.jsx
@@ -22,6 +22,27 @@ class SignIn extends Component {
         email: ""
     }
 
+    getLoginPasswordAndEmail = () => {
+        console.log("login:" + this.state.login);
+        console.log("password:" + this.state.password);
+        console.log("email:" + this.state.email);
+    }
+
+    loginChange = (event) => {
+        this.setState({login: event.target.value})
+        
+      }
+
+    passwordChange = (event) => {
+        this.setState({password: event.target.value})
+        
+      }
+
+    emailChange = (event) => {
+        this.setState({email: event.target.value})
+        
+      }
+
     render() {
 
         
@@ -29,13 +50,17 @@ class SignIn extends Component {
         <form style={this.stylesForm}>
             <label style={this.stylesLabel}>Create a new account:</label>
             <br/>
-            <input placeholder="Enter Your login..."></input>
+            <input type="text" name="login" placeholder="Enter Your login..." 
+            value={this.state.login} onChange={this.loginChange}></input>
             <br/>
-            <input placeholder="Enter Your email..."></input>
+            <input type="text" name="email" placeholder="Enter Your email..."
+            value={this.state.email} onChange={this.emailChange}></input>
             <br/>
-            <input placeholder="Enter Your password..."></input>
+            <input type="text" name="password" placeholder="Enter Your password..." 
+            value={this.state.password} onChange={this.passwordChange}></input>
             <br/>
-            <a  className="waves-effect waves-light btn">Sign in</a> 
+            <a  className="waves-effect waves-light btn" 
+            onClick={this.getLoginPasswordAndEmail}>Sign in</a> 
             <br/>
         </form>);
     }
