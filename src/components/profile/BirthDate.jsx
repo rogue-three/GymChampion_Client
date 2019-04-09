dc9f3a0409794913d456aa6f10cb05a5e243cc48
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-09 10:23:49 +0200

    Refactor BirthDate component

--- src/components/profile/BirthDate.jsx
+++ src/components/profile/BirthDate.jsx
@@ -4,29 +4,26 @@ class BirthDate extends Component {
   render() {
     const { dateCopy, checkNumber } = this.props;
     return (
-      <label className="row">
-        <span className="col s2" />
-        <span className="col s8 white-text teal lighten-1">BIRTH DATE</span>
+      <label>
+        <span />
+        <span>BIRTH DATE</span>
         <br />
-        <span className="col s2" />
+        <span />
         <input
-          className="col s2 input-field white center-align"
           type="text"
           name="day"
           placeholder={dateCopy.getDate().toString()}
           onKeyUp={checkNumber}
         />
-        <span className="col s1" />
+        <span />
         <input
-          className="col s2 input-field white center-align"
           type="text"
           name="month"
           placeholder={(dateCopy.getMonth() + 1).toString()}
           onKeyUp={checkNumber}
         />
-        <span className="col s1" />
+        <span />
         <input
-          className="col s2 input-field white center-align"
           type="text"
           name="year"
           placeholder={dateCopy.getFullYear().toString()}
