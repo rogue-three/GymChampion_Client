60dd699b24964a270d9c3710a8043a60a6f61f83
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-04-09 12:34:49 +0200

    Login component refactor.

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,52 +1,8 @@
 import React, { Component } from "react";
-
-import LogValidation from "./LogValidation";
-import SignUp from "./SignUp";
-import gym1 from "../../img/gym1.png";
-import Navbar from "../mainView/Navbar";
 import Logo from "./Logo";
-
 import LoginValidationForm from "./LoginValidationForm";
 
 class Login extends Component {
-
-  state = {
-    logIn: true,
-    signIn: false,
-    isLogged: false,
-    login:""
-  };
-
-  loginStyles = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-    paddingLeft: "5%",
-    paddingTop: "2%"
-  };
-
-  renderLogInOrSignIn = () => {
-    if (this.state.logIn)
-      return <LogValidation callbackFromParent={this.setIsLogged} />;
-    else if (this.state.signIn) return <SignUp />;
-  };
-
-
-
-  setLoginOnTrue = () => {
-    this.setState({ logIn: true });
-    this.setState({ signIn: false });
-  };
-
-  setSignInOnTrue = () => {
-    this.setState({ logIn: false });
-    this.setState({ signIn: true });
-  };
-
-  setIsLogged = logInfoFromValidator => {
-    this.setState({ isLogged: logInfoFromValidator });
-  };
 
   setLoginFromForm = (dataFromForm) => {
       this.props.callBackLogin(dataFromForm);
@@ -54,18 +10,13 @@ class Login extends Component {
   }
 
   render() {
-    if (this.state.isLogged) {
-      return <Navbar />;
-    } else {
+   
       return (
-        <span>
+        <React.Fragment>
             <Logo/>
             <LoginValidationForm callBackForm={this.setLoginFromForm}/>      
-            {/* <SignUp/> */}
-          <br />
-        </span>
+        </React.Fragment>
       );
     }
-  }
 }
 export default Login;
