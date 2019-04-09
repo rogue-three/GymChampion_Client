ef1308eece9148269e395c74aac36b3c10ef6591
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-04-10 01:25:11 +0200

    Add button to SingleExerciseComponent.

--- src/components/mainView/SingleExerciseScheme.jsx
+++ src/components/mainView/SingleExerciseScheme.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab';
+import Icon from '@material-ui/core/Icon';
 
 class SingleExerciseScheme extends Component {
 
     render() {
         return (
-            <Grid>
-                <Typography>{this.props.schemeName}</Typography>
-            </Grid>
+            <React.Fragment>
+                <Grid container direction="row" alignItems="center" justify="space-around" >
+                    <Typography>{this.props.schemeName}</Typography>
+                    <Fab color="primary" aria-label="Edit">
+                    </Fab>
+                </Grid>
+                <br/>
+                <br/>
+            </React.Fragment>
         );
     }
 }
