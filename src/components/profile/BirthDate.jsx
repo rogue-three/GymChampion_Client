4690cfc83b478ccdce806d40b9f7d88af7ce7545
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-09 10:16:33 +0200

    Refactor Weight component Initialize BirthDate component

---
+++ src/components/profile/BirthDate.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class BirthDate extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default BirthDate;
--- src/components/profile/Weight.jsx
+++ src/components/profile/Weight.jsx
@@ -3,13 +3,12 @@ import React, { Component } from "react";
 class Weight extends Component {
   render() {
     return (
-      <label className="row">
-        <span className="col s2" />
-        <span className="col s8 white-text teal lighten-1">BODY WEIGHT</span>
+      <label>
+        <span />
+        <span>BODY WEIGHT</span>
         <br />
-        <span className="col s5" />
+        <span />
         <input
-          className="col s2 input-field white center-align"
           type="text"
           name="weight"
           placeholder={this.props.weight}
