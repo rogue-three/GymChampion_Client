340768cd9cb992dc75d07961a9346d7477f0610a
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-10 16:12:05 +0200

    Create training_log component folder and initialize TrainingLog, Calendar, Log and LogEntry components

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import "./App.css";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import theme from "./themes/MuiComponentTheme";
-import Profile from "./components/profile/Profile";
+import TrainingLog from "./components/training_log/TrainingLog";
+// import Profile from "./components/profile/Profile";
 // import Navbar from "./components/mainView/Navbar";
 // import Login from "./components/login/Login";
 
 class App extends Component {
   renderLoginOrAppActiveUser() {
     // todo : logic to verify user token
-    return <Profile />;
+    return <TrainingLog />;
   }
 
   render() {
     return (
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        <Profile />
+        <TrainingLog />
         {/* <Navbar /> */}
       </MuiThemeProvider>
     );
---
+++ src/components/training_log/TrainingLog.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class TrainingLog extends Component {
+  state = {};
+  render() {
+    return <h1>HAHA</h1>;
+  }
+}
+
+export default TrainingLog;
