fb6e19923d7748b5263d464680b922f8c8d62c3d
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-10 16:32:39 +0200

    Implement fetch for user trainings

--- src/components/training_log/TrainingLog.jsx
+++ src/components/training_log/TrainingLog.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from "react";
 
 class TrainingLog extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      trainings: [],
+      login: "Mihu"
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:8080/v1/trainings/login/" + this.state.login)
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            trainings: result.map(training => ({
+              trainingId: training.trainingId,
+              trainingDateStart: training.trainingDateStart,
+              trainingDateFinish: training.trainingDateFinish,
+              userBodyWeight: training.userBodyWeight,
+              archived: training.archived,
+              user: {
+                login: training.user.login,
+                nickname: training.user.nickname,
+                birthDate: training.user.birthDate,
+                weight: training.user.weight,
+                gender: {
+                  genderId: training.user.gender.genderId,
+                  sex: training.user.gender.sex
+                }
+              }
+            }))
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
   render() {
-    return <h1>HAHA</h1>;
+    const { trainings } = this.state;
+    return (
+      <ul>
+        {trainings.map(training => (
+          <li key={training.trainingId}>{training.trainingId}</li>
+        ))}
+      </ul>
+    );
   }
 }
 
