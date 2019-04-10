31c8a9fd422907085d9a3b8365d482bbcdebb020
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-04-10 13:36:48 +0200

    Add ExerciseSchemeOptions component with data from particular ExerciseScheme (content and functionality to implement on future.

---
+++ src/components/mainView/ExerciseSchemeOptions.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+class ExerciseSchemeOptions extends Component {
+    
+    render() {
+        return (
+            <React.Fragment>
+                 <h1>{this.props.location.schemeName}</h1>
+            </React.Fragment>
+        )
+    }
+}
+/// ExerciseScheme options to implement.
+////Now this component render only passed data from link because
+// ExerciseSchemeOptions must know which ExerciseScheme will be handling
+export default ExerciseSchemeOptions;
