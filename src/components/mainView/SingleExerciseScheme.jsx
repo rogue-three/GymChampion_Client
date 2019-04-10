31e56f9aa4bdb16b910d0f7debc9ca86384c682f
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-04-10 12:52:07 +0200

    Implement passing data about chose exercise scheme to components on /moreoptions and /training routes.

--- src/components/mainView/SingleExerciseScheme.jsx
+++ src/components/mainView/SingleExerciseScheme.jsx
@@ -8,16 +8,25 @@ import { Link } from 'react-router-dom';
 
 class SingleExerciseScheme extends Component {
 
+
     render() {
+        const dataForTraining = {
+            pathname: "/training",
+            schemeName: this.props.schemeName
+        }
+        const dataForMoreOptions = {
+            pathname: "/moreoptions",
+            schemeName: this.props.schemeName
+        }
         return (
             <React.Fragment>
                 <Grid container style={{backgroundColor:'#80cbc4'}} direction="row" alignItems="center" justify="space-around" >
-                    <Button component={Link} to="/training">
+                    <Button component={Link} to={dataForTraining}>
                         <Typography style={{color:"white"}} variant="h6" gutterBottom>
                         {this.props.schemeName + "  TRAINING"}
                         </Typography>
                     </Button>
-                    <Fab component={Link} to="/moreoptions" size='small' color="primary" aria-label="Edit">
+                    <Fab component={Link} to={dataForMoreOptions} size='small' color="primary" aria-label="Edit">
                         <Icon>more_horiz</Icon>
                     </Fab>
                 </Grid>
