840f8c85cc1ab5c66fca7acd3cef4ef4ac2d58a6
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-11 20:32:20 +0200

    Implement fetch for exercise objects according to chosen option

--- src/components/training/Training.jsx
+++ src/components/training/Training.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
 import TrainingNavigation from "./TrainingNavigation";
 import SetSchemeHandler from "./SetSchemeHandler";
 import ExerciseHandler from "./ExerciseHandler";
@@ -7,15 +9,59 @@ class Training extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      schemeName: this.props.location.schemeName,
       exercises: []
     };
   }
+
+  componentDidMount() {
+    const schemeName = this.state.schemeName;
+    const schemeOption =
+      schemeName === undefined ? "" : "/exercise_scheme/" + schemeName;
+
+    fetch("http://localhost:8080/v1/exercises" + schemeOption)
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            exercises: result.map(exercise => ({
+              exerciseId: exercise.exerciseId,
+              exerciseName: exercise.exerciseName,
+              maxReps: exercise.maxReps,
+              maxWeight: exercise.maxWeight,
+              bodyPart: {
+                bodyPartId: exercise.bodyPart.bodyPartId,
+                bodyPartName: exercise.bodyPart.bodyPartName
+              },
+              equipment: {
+                equipmentId: exercise.equipment.equipmentId,
+                equipmentName: exercise.equipment.equipmentName
+              },
+              exerciseScheme: {
+                exerciseSchemeId: exercise.exerciseScheme.exerciseSchemeId,
+                exerciseSchemeName: exercise.exerciseScheme.exerciseSchemeName
+              }
+            }))
+          });
+        },
+        error => {
+          this.setState({
+            error
+          });
+        }
+      );
+  }
+
   render() {
-    const schemeName = this.props.location.schemeName;
-    const scheme = schemeName === undefined ? "Quick training" : schemeName;
+    const { schemeName, exercises } = this.state;
     return (
       <React.Fragment>
-        <h1>{scheme}</h1>
+        <h1>{schemeName}</h1>
+        <List>
+          {exercises.map(exercise => (
+            <ListItem key={exercise.exerciseId}>{exercise.exerciseId}</ListItem>
+          ))}
+        </List>
         <TrainingNavigation />
         <SetSchemeHandler />
         <ExerciseHandler />
