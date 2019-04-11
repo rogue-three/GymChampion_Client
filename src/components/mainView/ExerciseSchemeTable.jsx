67ebdc179a209d69a389328c43649231d6b4f9db
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-11 13:44:21 +0200

    Refactor list of options in MainView with material List

--- src/components/mainView/ExerciseSchemeTable.jsx
+++ src/components/mainView/ExerciseSchemeTable.jsx
@@ -1,31 +1,35 @@
 import React, { Component } from "react";
+import List from "@material-ui/core/List";
 import SingleExerciseScheme from "./SingleExerciseScheme";
 
 class ExerciseSchemeTable extends Component {
+  state = {
+    exerciseSchemes: [],
+    getAllSchemesURL:
+      "http://localhost:8080/v1/exercise_parameters/exercise_scheme"
+  };
 
-    state = {
-        exerciseSchemes: [],
-        getAllSchemesURL: "http://localhost:8080/v1/exercise_parameters/exercise_scheme"
-    }
-
-    componentDidMount() {
-      fetch(this.state.getAllSchemesURL)
-      .then(response=>response.json())
+  componentDidMount() {
+    fetch(this.state.getAllSchemesURL)
+      .then(response => response.json())
       .then(result => {
-        this.setState({exerciseSchemes: result});
+        this.setState({ exerciseSchemes: result });
       });
-    }
+  }
 
-    render() {
+  render() {
     return (
       <React.Fragment>
-        <ul>
-            {this.state.exerciseSchemes.map(scheme => <li key={scheme.exerciseSchemeName}>
-            <SingleExerciseScheme schemeName={scheme.exerciseSchemeName}/></li>)}
-        </ul>
-        </React.Fragment>
+        <List>
+          {this.state.exerciseSchemes.map(scheme => (
+            <li key={scheme.exerciseSchemeName}>
+              <SingleExerciseScheme schemeName={scheme.exerciseSchemeName} />
+            </li>
+          ))}
+        </List>
+      </React.Fragment>
     );
-    }
+  }
 }
 
 export default ExerciseSchemeTable;
