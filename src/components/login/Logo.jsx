7ad406cfeb10113dc5c30cd3dd45607c68c78511
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-11 13:39:12 +0200

    Add new vector logo and update Login controller

--- src/components/login/Logo.jsx
+++ src/components/login/Logo.jsx
@@ -1,22 +1,20 @@
-import React, { Component } from "react"; 
-import gym1 from "../../img/gym1.png";
-import Grid from '@material-ui/core/Grid';
-
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+import gym1 from "../../img/logo.svg";
+import Grid from "@material-ui/core/Grid";
 
 class Logo extends Component {
-
-    render() {
-        return (
-            <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            >
-                <img src={gym1} alt="logo" height="212" width="347" />
-            </Grid>
-        );
-    }
+  render() {
+    return (
+      <Grid container direction="row" justify="center" alignItems="center">
+        <img src={gym1} alt="logo" width="70%" />
+        <br />
+        <Typography>
+          <h2>GYM CHAMPION</h2>
+        </Typography>
+      </Grid>
+    );
+  }
 }
 
 export default Logo;
