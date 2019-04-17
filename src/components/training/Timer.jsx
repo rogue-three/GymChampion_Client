64e4a9b7f4eae4b6e0213c79a31545c8bfc1d32e
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-17 11:50:31 +0200

    Implement basic TrainingNavigation component structure Initialize Timer component

---
+++ src/components/training/Timer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Timer extends Component {
+  render() {
+    return <div>TIME</div>;
+  }
+}
+
+export default Timer;
--- src/components/training/TrainingNavigation.jsx
+++ src/components/training/TrainingNavigation.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Timer from "./Timer";
 
 class TrainingNavigation extends Component {
   state = {};
   render() {
-    return <div>Navigation</div>;
+    return (
+      <React.Fragment>
+        <Button>STOP</Button>
+        <Timer />
+        <Button>SAVE</Button>
+        <Button>CHANCEL</Button>
+      </React.Fragment>
+    );
   }
 }
 
