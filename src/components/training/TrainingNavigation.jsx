843cd68b04ee0d6e68e081e556e7b4df31d2c32f
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-17 11:56:10 +0200

    Wrap all subcomponents of a TrainingNavigation in Grid

--- src/components/training/TrainingNavigation.jsx
+++ src/components/training/TrainingNavigation.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import Timer from "./Timer";
+import Grid from "@material-ui/core/Grid";
 
 class TrainingNavigation extends Component {
   state = {};
   render() {
     return (
-      <React.Fragment>
-        <Button>STOP</Button>
-        <Timer />
-        <Button>SAVE</Button>
-        <Button>CHANCEL</Button>
-      </React.Fragment>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={2}>
+          <Button>STOP</Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Timer />
+        </Grid>
+        <Grid item xs={2}>
+          <Button>SAVE</Button>
+        </Grid>
+        <Grid item xs={2}>
+          <Button>CHANCEL</Button>
+        </Grid>
+      </Grid>
     );
   }
 }
