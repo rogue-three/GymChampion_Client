0ecb5f313a49a57402ac22ec60d55e1dd215db83
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-17 11:40:53 +0200

    Comment out StatsButton constructor

--- src/components/statistics/StatsButtons.jsx
+++ src/components/statistics/StatsButtons.jsx
@@ -1,31 +1,37 @@
 import React, { Component } from "react";
-import List from '@material-ui/core/List';
-import Button from '@material-ui/core/Button';
+import List from "@material-ui/core/List";
+import Button from "@material-ui/core/Button";
 
 class StatsButtons extends Component {
-    constructor(props) {
-        super(props)
-    }
+  // constructor(props) {
+  //     super(props)
+  // }
 
-    render() {
-        return (
-                    <List>
-                        {this.props.children}
-                        <Button
-                            variant="contained"
-                            // onClick={() => this.props.getGeneral()}
-                        >General</Button>
-                        <Button
-                            variant="contained"
-                            // onClick={this.props.getByExercise}
-                        >By Exercise</Button>
-                        <Button
-                            variant="contained"
-                            // onClick={this.props.getByBodyPart}
-                        >By Body Part</Button>
-                    </List>
-        );
-    }
+  render() {
+    return (
+      <List>
+        {this.props.children}
+        <Button
+          variant="contained"
+          // onClick={() => this.props.getGeneral()}
+        >
+          General
+        </Button>
+        <Button
+          variant="contained"
+          // onClick={this.props.getByExercise}
+        >
+          By Exercise
+        </Button>
+        <Button
+          variant="contained"
+          // onClick={this.props.getByBodyPart}
+        >
+          By Body Part
+        </Button>
+      </List>
+    );
+  }
 }
 
 export default StatsButtons;
