913486c5eb77b20df6a5f93a4150acb2246da05a
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-17 22:21:00 +0200

    Remove Timer component and move counter into TrainigNavigation

--- src/components/training/TrainingNavigation.jsx
+++ src/components/training/TrainingNavigation.jsx
@@ -1,24 +1,46 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
-import Timer from "./Timer";
 import Grid from "@material-ui/core/Grid";
 
 class TrainingNavigation extends Component {
   state = {
-    timerButtonValue: "START"
+    buttonValue: "START",
+    timerIsOn: false,
+    time: 0,
+    start: 0
   };
 
   changeTimerButtonValue = () => {
-    if (this.state.timerButtonValue === "START") {
-      console.log("was start");
-      this.setState({ timerButtonValue: "STOP" });
+    if (!this.state.timerIsOn) {
+      this.setState({ timerIsOn: true, buttonValue: "PAUSE" });
+      this.startTimer();
     } else {
-      console.log("was stop");
-      this.setState({ timerButtonValue: "START" });
+      this.setState({ timerIsOn: false, buttonValue: "START" });
+      this.stopTimer();
     }
   };
 
+  startTimer() {
+    this.state.start === 0
+      ? this.setState({ start: Date.now() })
+      : console.log("started before");
+
+    this.timer = setInterval(
+      () =>
+        this.setState({
+          time: this.state.time + 1
+        }),
+      1
+    );
+    console.log("counter started");
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+  }
+
   render() {
+    const { buttonValue } = this.state;
     return (
       <Grid
         container
@@ -34,12 +56,12 @@ class TrainingNavigation extends Component {
             color="primary"
             onClick={this.changeTimerButtonValue}
           >
-            {this.state.timerButtonValue}
+            {buttonValue}
           </Button>
         </Grid>
         <Grid item xs={1} />
         <Grid item xs={2}>
-          <Timer />
+          {this.state.time}
         </Grid>
         <Grid item xs={2}>
           <Button variant="contained" color="primary">
