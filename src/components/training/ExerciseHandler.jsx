f10a0129ef58620f505f6e3ad30355d554128b01
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-04-18 10:06:42 +0200

    Implement ExerciseHandler component

--- src/components/training/ExerciseHandler.jsx
+++ src/components/training/ExerciseHandler.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 class ExerciseHandler extends Component {
   render() {
     const { exercises } = this.props;
     return (
-      <List>
+      <Grid
+        container
+        direction="column"
+        justify="space-around"
+        alignItems="center"
+        spacing={24}
+      >
+        <Typography color="secondary">CLICK EXERCISE TO ADD A SET</Typography>
         {exercises.map(exercise => (
-          <ListItem key={exercise.exerciseId}>{exercise.exerciseId}</ListItem>
+          <Grid item key={exercise.exerciseId}>
+            <Button variant="contained">
+              {exercise.exerciseName.replace(/_/g, " ")}
+            </Button>
+          </Grid>
         ))}
-      </List>
+      </Grid>
     );
   }
 }
